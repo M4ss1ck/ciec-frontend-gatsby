167730dbaacce9e9a3a354b017f52fa6ded81e4f
Andy <4ndyraul@gmail.com>
2021-11-30 03:47:20 -0800

    bug en el toggle

--- src/components/readingProgress.js
+++ src/components/readingProgress.js
@@ -34,7 +34,7 @@ const ReadingProgress = ({ target }) => {
 
   return (
     <div
-      className={`sticky h-2 top-16 bg-gray-300 dark:bg-gray-700 z-40 mr-auto`}
+      className={`sticky h-2 top-16 bg-gray-300 dark:bg-secundario z-40 mr-auto`}
       style={{ width: `${readingProgress}%` }}
     />
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -43,13 +43,13 @@ const IndexPage = ({ data }) => (
       </p>
     </article>
     <article>
-      <h2 className="h2">Noticias</h2>
+      <h2 className="text-xl text-secundario font-bold mt-6">Noticias</h2>
       <ul className="flex flex-row flex-wrap items-center justify-evenly w-full">
         {data.allStrapiArticle.edges.map(document => (
           <Link
             key={document.node.id}
             to={`/noticias/${document.node.slug}`}
-            className="text-gray-600 dark:text-gray-500 hover:text-gray-300 hover:bg-primario dark:hover:text-primario group w-full md:w-1/3 lg:w-1/4 p-2 m-1 border-4 border-white hover:border-primario rounded-lg flex flex-col justify-center items-center"
+            className="text-gray-600 dark:text-gray-500 hover:text-gray-300 hover:bg-primario dark:hover:bg-gray-800 dark:hover:text-secundario group w-full md:w-1/3 lg:w-1/4 p-2 m-1 border-4 border-white hover:border-primario dark:hover:border-secundario rounded-lg flex flex-col justify-center items-center"
           >
             <li className="group-hover:border-primario rounded-lg flex flex-col justify-center items-center">
               <h2>{document.node.title}</h2>
